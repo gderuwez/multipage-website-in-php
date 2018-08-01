859808957c9dd8b3b3cd5ad6e7da3107d57fee0b
gderuwez <gderuwez@gmail.com>
2018-08-01 11:37:51 +0200

    logging seccesful

--- form-logs.php
+++ form-logs.php
@@ -3,7 +3,7 @@ $file = file_get_contents('./logs/logs.txt', true);
 $file = rtrim(trim($file), ',');
 $file = '[' . $file . ']';
 $log = json_decode($file, true);
-var_dump($log);
+
 foreach ($log as $key => $value) {
   echo '<pre>';
   echo $log[$key]['date'];
